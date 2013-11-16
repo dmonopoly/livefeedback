a395e67897d97389c8b35b7a0770a0da60d01c4d
David Zhang <dzhang21@gmail.com>
2013-11-15 23:07:01 -0800

    Made home url point to live_feedback index view

--- classroom/urls.py
+++ classroom/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-#     url(r'^$', include('live_feedback.urls')),
-    url(r'^live_feedback/', include('live_feedback.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^$', include('live_feedback.urls')),
+  url(r'^live_feedback/', include('live_feedback.urls', namespace="live_feedback")),
+  url(r'^polls/', include('polls.urls', namespace="polls")),
+  url(r'^admin/', include(admin.site.urls)),
 )
--- live_feedback/views.py
+++ live_feedback/views.py
@@ -5,16 +5,8 @@ from django.shortcuts import render, render_to_response
 
 def index(request):
   latest_poll_list = [1,2,3]
-  template = loader.get_template('live_feedback/index.html')
-  context = RequestContext(request, {
+  context = {
     'x': latest_poll_list,
-  })
-  return HttpResponse(template.render(context))
-#   my_list = [1,2,3]
-#   context = {'x': my_list}
-#   return render(request, 'live_feedback/index.html', context)
+  }
+  return render(request, 'live_feedback/index.html', context)
 
-#   return HttpResponse("whatsup");
-
-def home(request):
-    return render_to_response('index.html')
