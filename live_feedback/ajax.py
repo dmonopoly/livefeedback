53f84a5414861cfc3221c124a7aa6ac639ee09aa
David Zhang <dzhang21@gmail.com>
2013-11-17 00:07:25 -0800

    Tentative neutralization every 3 sec working.

--- live_feedback/ajax.py
+++ live_feedback/ajax.py
@@ -4,6 +4,7 @@ from dajaxice.decorators import dajaxice_register
 from live_feedback.models import Event
 
 STEP = .5
+NEUTRAL = 5  # Depends on range of gauge set in index.html
 
 
 @dajaxice_register
@@ -18,6 +19,16 @@ def increment(request):
 def decrement(request):
   e = Event.objects.all()[0]
   e.mood -= STEP
+  e.save()
+  return json.dumps({'mood': e.mood})
+
+@dajaxice_register
+def neutralizeByOneStep(request):
+  e = Event.objects.all()[0]
+  if e.mood < NEUTRAL:
+    e.mood += STEP
+  elif e.mood > NEUTRAL:
+    e.mood -= STEP
   e.save()
   return json.dumps({'mood': e.mood})
 
