ba4ccb20a2ebc47315f5c95260e9841035c84b06
David Zhang <dzhang21@gmail.com>
2013-11-16 23:30:29 -0800

    Added step factor

--- live_feedback/ajax.py
+++ live_feedback/ajax.py
@@ -3,11 +3,13 @@ from dajaxice.decorators import dajaxice_register
 
 from live_feedback.models import Event
 
+STEP = .5
+
 
 @dajaxice_register
 def increment(request):
   e = Event.objects.all()[0]
-  e.mood += 1
+  e.mood += STEP
   e.save()
   return json.dumps({'mood': e.mood})
 
@@ -15,7 +17,7 @@ def increment(request):
 @dajaxice_register
 def decrement(request):
   e = Event.objects.all()[0]
-  e.mood -= 1
+  e.mood -= STEP
   e.save()
   return json.dumps({'mood': e.mood})
 
