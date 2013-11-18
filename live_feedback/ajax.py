619cc40e893f8b61eaf709ff0ab118adf9485eaf
David Zhang <dzhang21@gmail.com>
2013-11-17 20:58:40 -0800

    Prevent from moving beyond min or max of gauge

--- live_feedback/ajax.py
+++ live_feedback/ajax.py
@@ -4,21 +4,26 @@ from dajaxice.decorators import dajaxice_register
 from live_feedback.models import Event
 
 STEP = .5
-NEUTRAL = 5  # Depends on range of gauge set in index.html
+# These values should correspond to those in range of gauge set in index.html:
+NEUTRAL = 5
+MIN = 0
+MAX = 10
 
 
 @dajaxice_register
 def increment(request):
   e = Event.objects.all()[0]
-  e.mood += STEP
-  e.save()
+  if e.mood < MAX:
+    e.mood += STEP
+    e.save()
   return json.dumps({'mood': e.mood})
 
 @dajaxice_register
 def decrement(request):
   e = Event.objects.all()[0]
-  e.mood -= STEP
-  e.save()
+  if e.mood > MIN:
+    e.mood -= STEP
+    e.save()
   return json.dumps({'mood': e.mood})
 
 @dajaxice_register
